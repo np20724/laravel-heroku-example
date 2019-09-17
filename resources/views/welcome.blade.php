2815519ad5cfe133c77e28cdd9daa11e59d10f34
np20724 <51689376+np20724@users.noreply.github.com>
2019-09-17 11:47:17 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,35 +20,28 @@
                 height: 100vh;
                 margin: 0;
             }
-
             .full-height {
                 height: 100vh;
             }
-
             .flex-center {
                 align-items: center;
                 display: flex;
                 justify-content: center;
             }
-
             .position-ref {
                 position: relative;
             }
-
             .top-right {
                 position: absolute;
                 right: 10px;
                 top: 18px;
             }
-
             .content {
                 text-align: center;
             }
-
             .title {
                 font-size: 84px;
             }
-
             .links > a {
                 color: #636b6f;
                 padding: 0 25px;
@@ -58,7 +51,6 @@
                 text-decoration: none;
                 text-transform: uppercase;
             }
-
             .m-b-md {
                 margin-bottom: 30px;
             }
@@ -75,15 +67,18 @@
                         <a href="{{ url('/register') }}">Register</a>
                     @endif
                 </div>
-<div class="title m-b-md">
-     Laravel
-</div>
-     
-<div classl="title m-b-md">
-     {{ date("d/m/y H:i:s") }}
-</div>   
-             
-             </div> <div class="links">
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Laravel
+                </div>
+
+                <div class="title m-b-md">
+                    {{ date("d/m/Y H:i:s") }}
+                </div>
+                
+                <div class="links">    
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
